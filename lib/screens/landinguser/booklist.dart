b1dc579a6b07da447bbe5c716283eef2c2d40768
AdamMoh <xlkartu12345@gmail.com>
2023-12-08 18:57:10 +0700

    add book list search bar

--- lib/screens/landinguser/booklist.dart
+++ lib/screens/landinguser/booklist.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api, prefer_const_literals_to_create_immutables, unnecessary_string_interpolations
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api, prefer_const_literals_to_create_immutables, unnecessary_string_interpolations, unused_field, avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -15,6 +15,30 @@ class BookPage extends StatefulWidget {
 }
 
 class _BookPageState extends State<BookPage> {
+  List<Books> _books = [];
+  TextEditingController searchController = TextEditingController();
+
+  Future<List<Books>> searchBooks(String query) async {
+    var url = Uri.parse('http://localhost:8000/flutter/searchbooks/');
+    var uri = Uri.http(url.authority, url.path, {"search_term": query});
+
+    var response = await http.get(
+      uri,
+      headers: {"Content-Type": "application/json"},
+    );
+
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    List<Books> listBooks = [];
+    for (var d in data) {
+      if (d != null) {
+        listBooks.add(Books.fromJson(d));
+      }
+    }
+    listBooks.sort((a, b) => b.fields.rating.compareTo(a.fields.rating));
+    return listBooks;
+  }
+
   Future<List<Books>> fetchBooks() async {
     var url = Uri.parse('http://localhost:8000/loadbooks/');
     var response = await http.get(
@@ -43,73 +67,112 @@ class _BookPageState extends State<BookPage> {
         foregroundColor: Colors.amber,
       ),
       drawer: LeftDrawer(),
-      body: FutureBuilder(
-        future: fetchBooks(),
-        builder: (context, AsyncSnapshot<List<Books>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "No books available.",
-                    style: TextStyle(color: Colors.amber, fontSize: 20),
-                  ),
-                  SizedBox(height: 8),
-                ],
-              ),
-            );
-          } else {
-            return GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 5, // Jumlah kartu per baris
-                crossAxisSpacing: 16.0,
-                mainAxisSpacing: 16.0,
-              ),
-              itemCount: snapshot.data!.length,
-              itemBuilder: (_, index) => GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          BookDetailPage(book: snapshot.data![index]),
-                    ),
-                  );
-                },
-                child: Card(
-                  color: Colors.black87,
-                  margin: const EdgeInsets.all(8.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "${snapshot.data![index].fields.name}",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.amber,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                        ),
-                      ],
-                    ),
-                  ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              onChanged: (String value) async {
+                print('Search text changed: $value');
+                if (value.isEmpty) {
+                  List<Books> allBooks = await fetchBooks();
+                  setState(() {
+                    _books = allBooks;
+                  });
+                } else {
+                  try {
+                    List<Books> searchedBooks = await searchBooks(value);
+                    setState(() {
+                      _books = searchedBooks;
+                    });
+                  } catch (error) {
+                    print('Error during search: $error');
+                  }
+                }
+              },
+              decoration: InputDecoration(
+                hintText: 'Search books...',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () async {},
                 ),
               ),
-            );
-          }
-        },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _books.isEmpty && searchController.text.isEmpty
+                  ? fetchBooks()
+                  : searchBooks(searchController.text),
+              builder: (context, AsyncSnapshot<List<Books>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "No books available.",
+                          style: TextStyle(color: Colors.amber, fontSize: 20),
+                        ),
+                        SizedBox(height: 8),
+                      ],
+                    ),
+                  );
+                } else {
+                  return GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 5, // Jumlah kartu per baris
+                      crossAxisSpacing: 16.0,
+                      mainAxisSpacing: 16.0,
+                    ),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (_, index) => GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                BookDetailPage(book: snapshot.data![index]),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        color: Colors.black87,
+                        margin: const EdgeInsets.all(8.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "${snapshot.data![index].fields.name}",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.amber,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
