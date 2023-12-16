05e94cf9ba084e1a8b9d196839100add41351de8
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2023-12-16 15:28:19 +0700

    commit

--- lib/screens/review/review_form.dart
+++ lib/screens/review/review_form.dart
@@ -153,7 +153,7 @@ class _ReviewFormPageState extends State<ReviewFormPage> {
                     onPressed: () async {
                     if (_formKey.currentState!.validate()) {
                       // Send request to Django and wait for the response
-                      final response = await request.post(
+                      final response = await request.postJson(
                           "http://127.0.0.1:8000/review/add-review-flutter/",
                           jsonEncode(<String, String>{
                             'reviewTitle': _review_title,
